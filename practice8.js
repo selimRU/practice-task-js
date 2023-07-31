5542b3ad19bf6b4765d73f96033b749006ff70ec
selimRU <selim.soha92@gmail.com>
2023-07-31 19:46:15 +0600

    practice8 added

--- practice8.js
+++ practice8.js
@@ -1,7 +1,7 @@
 var prices = [10, 20, 30, 50, 80, 90, 150, 200, 300, 400, 500,]
-for(let i = 0; i <= prices.length; i++){
+for(let i = 0; i < prices.length; i++){
     var price = prices[i]
-    if(price > 200){
+    if(price >=200){
         continue
     }
     console.log(price);

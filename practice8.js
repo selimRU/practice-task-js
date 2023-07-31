0fc439bf0e94d837f5f723cb62b671e53cd22e7c
selimRU <selim.soha92@gmail.com>
2023-07-31 19:40:28 +0600

    practice8 added

---
+++ practice8.js
@@ -0,0 +1,8 @@
+var prices = [10, 20, 30, 50, 80, 90, 150, 200, 300, 400, 500,]
+for(let i = 0; i <= prices.length; i++){
+    var price = prices[i]
+    if(price > 200){
+        continue
+    }
+    console.log(price);
+}

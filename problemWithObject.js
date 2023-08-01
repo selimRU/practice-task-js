ffe0bed072c7a9124ef3f8890c3ca34441519339
selimRU <selim.soha92@gmail.com>
2023-08-02 00:19:45 +0600

    problem added

---
+++ problemWithObject.js
@@ -0,0 +1,48 @@
+// function foo(){
+// console.log('foo');
+// console.log(bar());
+// }
+// foo()
+
+
+// function bar(){
+// console.log('bar');
+// }
+// bar()
+
+// function make_avg(num1, num2, num3) {
+//     var avarage = (num1 + num2 + num3) / 3;
+//     return avarage;
+// }
+
+// var finalResult = make_avg(20, 30, 40);
+// console.log(finalResult);
+
+
+//way-1
+// function odd_even(number) {
+//     if (number % 2 === 0) {
+//         console.log('even');
+//     }
+//     else {
+//         console.log('odd');
+//     }
+// }
+//  var result = odd_even(60)
+ var number = 60;
+switch(number){
+    case 41:
+        console.log('odd');
+        break;
+    case 51:
+        console.log('odd');
+        break;
+    case 63:
+        console.log('even');
+        break;
+    case 61:
+        console.log('odd');
+        break;
+        default:
+            console.log('kuhje paini');
+}

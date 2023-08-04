d685461e944c4cee32bd1f8739b8ab49e7818564
selimRU <selim.soha92@gmail.com>
2023-08-04 13:53:32 +0600

    largest number

--- largestNumber.js
+++ largestNumber.js
@@ -1,10 +1,25 @@
+// function largestNumberSelection(array) {
+//     let largestNumber = array[0];
+//     for (let i = 0; i < array.length; i++) {
+//         let number = array[i];
+//         if (number > largestNumber) {
+//             largestNumber = number;
+//         }
+//     }
+//     return largestNumber;
+// }
+// let arr = [2, 11, 5, 1, 7, 9, 3, 42];
+// let result = largestNumberSelection(arr);
+// console.log(result);
 function largestNumberSelection(array) {
     let largestNumber = array[0];
-    for (let i = 0; i < array.length; i++) {
+    let i = 0;
+    while (i < array.length) {
         let number = array[i];
         if (number > largestNumber) {
             largestNumber = number;
         }
+        i++
     }
     return largestNumber;
 }
